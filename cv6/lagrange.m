64865dd7f8e548bd0fbff43dd1eb562209b5c8d5
robert <robert.hak@tul.cz>
2022-05-10 14:34:57 +0200

    shit

--- cv6/lagrange.m
+++ cv6/lagrange.m
@@ -1,19 +1,25 @@
 function yq = lagrange(x,y,xq)
-    n=size(x,2);
-    size(x, 2)
-    I=ones(n,size(xq,2));
+    xcount = numel(x);
+    yq = zeros(numel(xq), 1);
+    
+    for h=1:numel(xq)
+        
+        n = xq(h);
 
-    for i=1:n
-      for j=1:n
-         if (i~=j)
-            I(i,:)=I(i,:).*((xq-x(j))/(x(i)-x(j)));
-         end
-      end
+        for i=1:xcount
+            foo = 1;           
+            for j=1:xcount
+                if i ~= j
+                    foo = foo * (n - x(j)) / (x(i) - x(j));
+                end
+            end
+            yq(h) = yq(h) + (y(i) * foo);
+        end
+
     end
-    yq=0;
-   for i=1:n
-      yq=yq+y(i)*I(i,:);
-   end
+    
+    % overeni jestli se jedna o sloupcovy vektor
+    if (iscolumn(yq))
+        yq = yq';
+    end
 end
-
-
