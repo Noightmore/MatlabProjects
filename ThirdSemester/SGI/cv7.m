7d6171b632794289dfec2b53e149bf9cf72e3c15
robert <robert.hak@tul.cz>
2022-11-10 09:50:46 +0100

    sus

--- ThirdSemester/SGI/cv7.m
+++ ThirdSemester/SGI/cv7.m
@@ -5,21 +5,23 @@ N1 = 10;
 N2 = 20;
 
 Fs = 10;
-t = 1:1/Fs:2-1/Fs; % ne do fs ale do N
-
-
-
+t = 1:1/Fs:2-1/Fs; 
+t2 = 1:1/(2*Fs):2; % idk
 
 F1 = 4;
 F2 = 2.5;
 F3 = 12;
 F4 = 7.25;
 
-
 f1 = cos(2*pi * F1 * t);
 f2 = cos(2*pi*F2*t);
 f3 = cos(2*pi*F3 * t);
 f4 = cos(2*pi*F4 * t);
+
+f11 = cos(2*pi * F1 * t2);
+f22 = cos(2*pi*F2*t2);
+f33 = cos(2*pi*F3 * t2);
+f44 = cos(2*pi*F4 * t2);
 
 X1 = abs(DFT(f1, N1));
 
@@ -27,13 +29,17 @@ X2 = abs(DFT(f2, N1));
 X3 = abs(DFT(f3, N1));
 X4 = abs(DFT(f4, N1));
 
-% X11 = abs(DFT(f1, N2));
-% X22 = abs(DFT(f2, N2));
-% X33 = abs(DFT(f3, N2));
-% X44 = abs(DFT(f4, N2));
+% dft pro N2 susuje
+X11 = abs(DFT(f11, N2));
+X22 = abs(DFT(f22, N2));
+X33 = abs(DFT(f33, N2));
+X44 = abs(DFT(f44, N2));
 % 
 subplot(2,4, 1);
 stem(t, X1);
+
+subplot(2, 4, 2);
+stem(t2, X11);
 
 subplot(2, 4, 3);
 stem(t ,X2);
